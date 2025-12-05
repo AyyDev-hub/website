9ae4ab1709b8cd2f4e36af13e307becb95f40179
AyyDev <ayydev22@gmail.com>
2025-12-05 11:44:51 +0700

    Update navigation.js

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -11,4 +11,17 @@ const nav = document.getElementById("nav");
 pages.forEach(p => {
   nav.innerHTML += `<li><a href="${p.link}">${p.name}</a></li>`;
 });
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+const menuOverlay = document.getElementById('menuOverlay');
+
+function toggleMenu() {
+  navMenu.classList.toggle('active');
+  menuOverlay.classList.toggle('show');
+  document.body.classList.toggle('no-scroll');
+}
+
+hamburger.addEventListener('click', toggleMenu);
+menuOverlay.addEventListener('click', toggleMenu);
 </script>
